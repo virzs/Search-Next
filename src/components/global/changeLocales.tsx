40709582de641bc786ba35895bc61d1e48f8570b
virzs <2369310227@qq.com>
2021-03-19 15:39:20 +0800

    多语言配置，及切换组件

---
+++ src/components/global/changeLocales.tsx
@@ -0,0 +1,56 @@
+/*
+ * @Author: Vir
+ * @Date: 2021-03-16 14:58:48
+ * @Last Modified by: Vir
+ * @Last Modified time: 2021-03-16 16:30:58
+ */
+
+import { getLocale, setLocale } from '@/.umi/plugin-locale/localeExports';
+import { selectLocalesValue } from '@/locales/locale';
+import { MenuItem, Button, Menu } from '@material-ui/core';
+import { Public } from '@material-ui/icons';
+import React from 'react';
+
+const ChangeLocales: React.FC = () => {
+  const [localeValue, setLocaleValue] = React.useState<string>(getLocale());
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = (value: string) => {
+    setAnchorEl(null);
+    setLocaleValue(value);
+    setLocale(value, false);
+  };
+
+  return (
+    <>
+      <Button onClick={handleClick}>
+        <Public fontSize="small" />
+        {selectLocalesValue.find((i) => i.value === localeValue)?.label}
+      </Button>
+      <Menu
+        id="locales-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        {selectLocalesValue.map((i) => (
+          <MenuItem
+            selected={localeValue === i.value}
+            value={i.value}
+            key={i.value}
+            onClick={() => handleClose(i.value)}
+          >
+            {i.label}
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
+  );
+};
+
+export default ChangeLocales;
--- src/locales/locale.ts
+++ src/locales/locale.ts
@@ -2,7 +2,7 @@
  * @Author: Vir
  * @Date: 2021-03-14 17:34:17
  * @Last Modified by: Vir
- * @Last Modified time: 2021-03-14 19:46:44
+ * @Last Modified time: 2021-03-16 15:15:44
  */
 
 export interface LocaleTypes {
@@ -11,10 +11,31 @@ export interface LocaleTypes {
   enUS: string;
 }
 
+export interface selectLocalesTypes {
+  label: string;
+  value: string;
+}
+
+export const selectLocalesValue: selectLocalesTypes[] = [
+  {
+    label: '中文',
+    value: 'zh-CN',
+  },
+  {
+    label: 'English',
+    value: 'en-US',
+  },
+];
+
 export default [
   {
-    id: 'SEARCH',
+    id: 'MAIN_SEARCH',
     zhCN: '搜索',
     enUS: 'Search',
+  },
+  {
+    id: 'MAIN_SEARCH_PLACEHOLDER',
+    zhCN: '请输入搜索内容',
+    enUS: 'Please enter search content',
   },
 ] as LocaleTypes[];
