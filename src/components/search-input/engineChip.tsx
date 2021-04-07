8a726702ee10cd1344ae323522fe76a0c17968cc
virzs <2369310227@qq.com>
2021-04-07 16:24:30 +0800

    refactor 取消选择引擎部分操作

--- src/components/search-input/engineChip.tsx
+++ src/components/search-input/engineChip.tsx
@@ -7,11 +7,9 @@
 
 import classnames from 'classnames';
 import { Chip } from '@material-ui/core';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SearchEngineValueTypes } from '@/data/engine';
 import { list } from '@/apis/search';
-import { ArrowLeft, ArrowRight } from '@material-ui/icons';
-import { animated, useSpring } from 'react-spring';
 import './style/chip.less';
 
 export type EngineChipType = '' | '';
@@ -19,23 +17,11 @@ export interface EngineChipPropTypes {
   // onClick?: () => void;
   onChange?: (value: SearchEngineValueTypes) => void;
   type?: EngineChipType; // 选择搜索引擎组件展示类型
-  autoHidden?: boolean; // 是否选择后自动隐藏
-}
-
-interface ListType {
-  clientWidth: number;
-  clientHeight: number;
 }
 
 const EngineChip = ({ onChange }: EngineChipPropTypes) => {
   const [engineList, setEngineList] = useState([] as SearchEngineValueTypes[]);
   const [engine, setEngine] = useState({} as SearchEngineValueTypes);
-  const [isExpand, setIsExpand] = useState<boolean>(false); // 是否展开
-  const [listEle, setListEle] = useState({} as ListType);
-  const [chipBtnWidth, setChipBtnWidth] = useState<number>(20); //当前按钮宽度
-  const [engineWidth, setEngineWidth] = useState<number>(0); // 当前选中的宽度
-  const ele = useRef(null); // chip组件整体ref
-  const btn = useRef(null); // chip组件展开收缩按钮ref
 
   // 获取搜索引擎列表
   const getList = () => {
@@ -56,82 +42,27 @@ const EngineChip = ({ onChange }: EngineChipPropTypes) => {
 
   const changeEngine = (item: SearchEngineValueTypes, e: any) => {
     localStorage.setItem('engine_id', item.id);
-    setEngineWidth(e.target.clientWidth);
     setEngine(item);
     if (onChange) onChange(item);
-  };
-
-  const buttonClick = () => {
-    setIsExpand(!isExpand);
-    const width =
-      engineWidth !== 0 ? engineWidth + 26 : localStorage.getItem('engine_w');
-    setChipBtnWidth(isExpand ? Number(width) : 16);
   };
 
   useEffect(() => {
     getList();
   }, []);
 
-  useEffect(() => {
-    if (ele && ele.current) {
-      // 此处设置了一个延迟用于获取正确的信息，后续有更好的方法再修改
-      const current: any = ele.current;
-      setTimeout(() => {
-        setListEle({
-          clientWidth: current.clientWidth,
-          clientHeight: current.clientHeight,
-        });
-      }, 1);
-    }
-    if (btn && btn.current) {
-      const btnCurrent: any = btn.current;
-      setTimeout(() => {
-        setChipBtnWidth(btnCurrent.clientWidth);
-        localStorage.setItem('engine_w', btnCurrent.clientWidth);
-      }, 1);
-    }
-  }, [ele, btn]);
-
   return (
-    <div
-      className="search-engine-label"
-      style={{ height: listEle.clientHeight }}
-    >
-      <animated.div
-        className="engine-list"
-        style={useSpring({
-          to: { left: isExpand ? '0px' : `-${listEle.clientWidth || 0}px` },
-        })}
-        ref={ele}
-      >
-        {engineList.map((i) => (
-          <Chip
-            key={i.id}
-            className={classnames('engine-chip', {
-              selected: i.id === engine.id,
-            })}
-            size="small"
-            label={i.name}
-            onClick={(e) => changeEngine(i, e)}
-          ></Chip>
-        ))}
-        <div
-          ref={btn}
-          className="chip-btn"
-          onClick={buttonClick}
-          style={{ right: `-${chipBtnWidth}px` }}
-        >
-          <Chip
-            className="engine-chip selected"
-            size="small"
-            label={engine.name}
-            style={{ display: isExpand ? 'none' : '' }}
-          ></Chip>
-          <div className="svg-btn">
-            {isExpand ? <ArrowLeft></ArrowLeft> : <ArrowRight></ArrowRight>}
-          </div>
-        </div>
-      </animated.div>
+    <div className="search-engine-label">
+      {engineList.map((i) => (
+        <Chip
+          key={i.id}
+          className={classnames('engine-chip', {
+            selected: i.id === engine.id,
+          })}
+          size="small"
+          label={i.name}
+          onClick={(e) => changeEngine(i, e)}
+        ></Chip>
+      ))}
     </div>
   );
 };
