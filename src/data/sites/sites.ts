419ad1dd8f68a67807429f3efd108d73d504dec3
virzs <2369310227@qq.com>
2021-04-10 21:50:56 +0800

    feat 网站数据类型

---
+++ src/data/sites/sites.ts
@@ -0,0 +1,17 @@
+/*
+ * @Author: Vir
+ * @Date: 2021-04-10 21:49:18
+ * @Last Modified by: Vir
+ * @Last Modified time: 2021-04-10 21:50:22
+ */
+
+export interface SitesValueTypes {
+  id: string;
+  name: string;
+  url: string;
+  icon: string;
+  color: string;
+  isShow: boolean;
+}
+
+export default [{}] as SitesValueTypes[];
