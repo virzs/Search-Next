9796a20f6fa794cb5aadfe836d10724360da4e3b
virzs <2369310227@qq.com>
2021-04-11 22:50:34 +0800

    chore site dialog

--- src/components/top-sites/dialog.tsx
+++ src/components/top-sites/dialog.tsx
@@ -2,15 +2,24 @@
  * @Author: Vir
  * @Date: 2021-04-10 21:52:45
  * @Last Modified by: Vir
- * @Last Modified time: 2021-04-10 22:20:08
+ * @Last Modified time: 2021-04-11 22:44:20
  */
 
-import { Dialog, DialogTitle, DialogContent } from '@material-ui/core';
+import {
+  Dialog,
+  DialogContent,
+  TextField,
+  DialogActions,
+  Button,
+} from '@material-ui/core';
+import React from 'react';
 import { useIntl } from 'react-intl';
+import { DialogTitle } from '../material-ui-custom/dialog';
+import './styles/dialog.less';
 
 // 网址 新增、编辑弹窗
 
-type SiteDialogType = 'add' | 'edit' | 'del';
+export type SiteDialogType = 'add' | 'edit' | 'del';
 
 interface SiteDialogPropTypes {
   open: boolean;
@@ -20,28 +29,67 @@ interface SiteDialogPropTypes {
 
 const SiteDialog: React.FC<SiteDialogPropTypes> = ({ open, type, onClose }) => {
   const { formatMessage } = useIntl();
-  const showTitleByType = new Map([
-    ['add', formatMessage({ id: 'app.component.sitedialog.title.add' })],
-    ['edit', formatMessage({ id: 'app.component.sitedialog.title.edit' })],
-    ['del', formatMessage({ id: 'app.component.sitedialog.title.del' })],
-  ]);
 
-  const addContent = () => {};
+  const addContent = () => {
+    return (
+      <form autoComplete="off">
+        <TextField
+          variant="outlined"
+          fullWidth
+          required
+          size="small"
+          style={{ margin: '5px 0' }}
+          label={formatMessage({ id: 'app.component.sitedialog.form.name' })}
+          placeholder={formatMessage({
+            id: 'app.component.sitedialog.placeholder.name',
+          })}
+        ></TextField>
+        <TextField
+          variant="outlined"
+          fullWidth
+          required
+          size="small"
+          style={{ margin: '5px 0' }}
+          label={formatMessage({ id: 'app.component.sitedialog.form.url' })}
+          placeholder={formatMessage({
+            id: 'app.component.sitedialog.placeholder.url',
+          })}
+        ></TextField>
+      </form>
+    );
+  };
 
-  const editContent = () => {};
+  const editContent = () => {
+    return <></>;
+  };
 
-  const delContent = () => {};
+  const delContent = () => {
+    return <></>;
+  };
 
-  const showContentByType = new Map([
-    ['add', addContent],
-    ['edit', editContent],
-    ['del', delContent],
-  ]);
+  React.useEffect(() => {}, [open]);
 
   return (
-    <Dialog open={open} onClose={onClose}>
-      <DialogTitle>{showTitleByType.get(type)}</DialogTitle>
-      <DialogContent>{showContentByType.get(type)}</DialogContent>
+    <Dialog className="top-site-dialog" open={open} onClose={onClose}>
+      <DialogTitle onClose={onClose}>
+        {type === 'add' &&
+          formatMessage({ id: 'app.component.sitedialog.title.add' })}
+        {type === 'edit' &&
+          formatMessage({ id: 'app.component.sitedialog.title.edit' })}
+        {type === 'del' &&
+          formatMessage({ id: 'app.component.sitedialog.title.del' })}
+      </DialogTitle>
+      <DialogContent>
+        {type === 'add' && addContent()}
+        {type === 'edit' && editContent()}
+        {type === 'del' && delContent()}
+      </DialogContent>
+      <DialogActions className="dialog-actions">
+        <Button variant="contained">取消</Button>
+        <Button variant="contained" color="primary">
+          确认
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 };
--- src/components/top-sites/index.tsx
+++ src/components/top-sites/index.tsx
@@ -2,14 +2,45 @@
  * @Author: Vir
  * @Date: 2021-04-10 21:33:12
  * @Last Modified by: Vir
- * @Last Modified time: 2021-04-10 21:37:42
+ * @Last Modified time: 2021-04-11 15:10:44
  */
+import { Grid } from '@material-ui/core';
+import React from 'react';
+import SiteDialog, { SiteDialogType } from './dialog';
+import SiteCard from './siteCard';
 import './styles/index.less';
 
 export interface TopSitesPropTypes {}
 
 const TopSites: React.FC = () => {
-  return <></>;
+  const [open, setOpen] = React.useState<boolean>(false);
+  const [type, setType] = React.useState<SiteDialogType>('add');
+
+  const addClick = () => {
+    setType('add');
+    setOpen(true);
+  };
+
+  const dialogClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Grid
+        className="top-sites"
+        container
+        alignContent="center"
+        alignItems="center"
+        justify="center"
+      >
+        <Grid item>
+          <SiteCard type="add" onClick={addClick} />
+        </Grid>
+      </Grid>
+      <SiteDialog open={open} type={type} onClose={dialogClose} />
+    </>
+  );
 };
 
 export default TopSites;
