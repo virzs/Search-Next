7abed3cd275a68731c6926ef3fa3ccffcd63ae90
virzs <2369310227@qq.com>
2021-04-11 22:49:03 +0800

    feat material-ui自定义组件全局导出

---
+++ src/components/material-ui-custom/dialog/index.tsx
@@ -0,0 +1,11 @@
+/*
+ * @Author: Vir
+ * @Date: 2021-04-11 19:59:14
+ * @Last Modified by: Vir
+ * @Last Modified time: 2021-04-11 22:41:09
+ */
+import DialogTitleCustom from './dialog-title';
+
+const DialogTitle = DialogTitleCustom;
+
+export { DialogTitle };
