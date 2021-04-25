fef84e84273705e1e2e5cddbf9297b7b59890308
virzs <2369310227@qq.com>
2021-04-25 16:29:26 +0800

    chore Snackbar组件

--- src/components/material-ui-custom/notistack/index.tsx
+++ src/components/material-ui-custom/notistack/index.tsx
@@ -5,8 +5,10 @@
  * @Last Modified time: 2021-04-23 16:17:59
  */
 
-import { SnackbarOrigin } from '@material-ui/core';
-import { SnackbarProvider } from 'notistack';
+import { Button, IconButton, SnackbarOrigin } from '@material-ui/core';
+import { Close } from '@material-ui/icons';
+import { SnackbarKey, SnackbarProvider } from 'notistack';
+import React from 'react';
 
 // 最大数量
 const MAX_SNACK: number = 3;
@@ -14,22 +16,60 @@ const MAX_SNACK: number = 3;
 const AUTO_HIDE_DURATION: number = 3000;
 // 消息条位置，默认右上角
 const POSITION: SnackbarOrigin = {
-  vertical: 'top',
+  vertical: 'bottom',
   horizontal: 'right',
 };
+// 隐藏图标
+const HIDE_ICON = false;
+// 是否防止重复，false可显示多条
+const PREVENT_DUPLICATE = false;
 
 interface NotistackPropsType {
   children: React.ReactNode;
 }
 
-const NotistackWrapper = ({ children }: NotistackPropsType) => (
-  <SnackbarProvider
-    maxSnack={MAX_SNACK}
-    autoHideDuration={AUTO_HIDE_DURATION}
-    anchorOrigin={POSITION}
-  >
-    {children}
-  </SnackbarProvider>
-);
+type refType =
+  | ((
+      | string
+      | React.RefObject<SnackbarProvider>
+      | ((instance: SnackbarProvider | null) => void)
+    ) &
+      (
+        | React.RefObject<SnackbarProvider>
+        | ((instance: SnackbarProvider | null) => void)
+      ))
+  | null
+  | undefined;
+
+const NotistackWrapper = ({ children }: NotistackPropsType) => {
+  const notistackRef: refType = React.createRef();
+  const onClickDismiss = (key: SnackbarKey, ref: SnackbarProvider) => () => {
+    ref.closeSnackbar(key);
+  };
+  return (
+    <SnackbarProvider
+      maxSnack={MAX_SNACK}
+      autoHideDuration={AUTO_HIDE_DURATION}
+      anchorOrigin={POSITION}
+      hideIconVariant={HIDE_ICON}
+      preventDuplicate={PREVENT_DUPLICATE}
+      ref={notistackRef}
+      action={(key) =>
+        notistackRef.current && (
+          <IconButton
+            aria-label="close"
+            color="inherit"
+            size="small"
+            onClick={onClickDismiss(key, notistackRef.current)}
+          >
+            <Close fontSize="inherit" />
+          </IconButton>
+        )
+      }
+    >
+      {children}
+    </SnackbarProvider>
+  );
+};
 
 export default NotistackWrapper;
