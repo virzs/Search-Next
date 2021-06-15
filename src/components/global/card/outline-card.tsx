86f08dac067bb08362d573aa8d29c551137ce09d
virzs <2369310227@qq.com>
2021-06-15 17:33:20 +0800

    chore outline card 支持loading、disabled

--- src/components/global/card/outline-card.tsx
+++ src/components/global/card/outline-card.tsx
@@ -2,20 +2,23 @@
  * @Author: Vir
  * @Date: 2021-06-10 16:55:48
  * @Last Modified by: Vir
- * @Last Modified time: 2021-06-15 13:57:09
+ * @Last Modified time: 2021-06-15 17:25:44
  */
 
 import { Tooltip } from '@material-ui/core';
+import { Skeleton } from '@material-ui/lab';
 import classNames from 'classnames';
 import React from 'react';
 import './styles/outline-card.style.less';
 
 export interface OutlineCardProps {
-  label?: string; //标题
+  label?: React.ReactNode; //标题
   id?: string; //当前值
   value?: string; //选中值
   onChange?: (val: string) => void; //change事件，选中返回id，否则返回空
-  tip?: string;
+  tip?: string; //提示tooltip文案
+  disabled?: boolean; //是否禁用
+  loading?: boolean; //是否加载中，显示skeleton
 }
 
 const OutlineCard: React.FC<OutlineCardProps> = ({
@@ -23,6 +26,8 @@ const OutlineCard: React.FC<OutlineCardProps> = ({
   id,
   value,
   tip,
+  disabled = false,
+  loading = false,
   children,
   onChange,
 }) => {
@@ -50,26 +55,37 @@ const OutlineCard: React.FC<OutlineCardProps> = ({
         checked={radioChecked}
         onChange={() => {}}
         onClick={() => setChecked(true)}
+        disabled={disabled}
       />
     );
   };
 
   return (
     <div className="outline-card-root">
-      <div className="outline-card-container">
-        <div className="outline-card-container-flex">
-          {tip ? <Tooltip title={tip}>{radio()}</Tooltip> : radio()}
-          <label slot="label" htmlFor={id} className="outline-card-label">
-            <div className="outline-card-label-content">
-              <div className="outline-card-content">{children}</div>
-              {label && (
-                <div>
-                  <label className="outline-card-title">{label}</label>
-                </div>
+      <div className="outline-card-container-flex">
+        {tip ? <Tooltip title={tip}>{radio()}</Tooltip> : radio()}
+        <label slot="label" htmlFor={id} className="outline-card-label">
+          <div className="outline-card-label-content">
+            <div className="outline-card-content">
+              {loading ? (
+                <Skeleton variant="rect" width={150} height={84} />
+              ) : (
+                children
               )}
             </div>
-          </label>
-        </div>
+            {label && (
+              <div>
+                <label className="outline-card-title">
+                  {loading ? (
+                    <Skeleton variant="text" width={100} height={16} />
+                  ) : (
+                    label
+                  )}
+                </label>
+              </div>
+            )}
+          </div>
+        </label>
       </div>
     </div>
   );
