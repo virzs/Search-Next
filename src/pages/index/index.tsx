587b6855876d3b68b9c7a482e15ae98e102ee910
virzs <2369310227@qq.com>
2021-09-23 16:51:34 +0800

    :sparkles: 首页支持设置背景

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,41 +2,68 @@
  * @Author: Vir
  * @Date: 2021-03-14 15:22:13
  * @Last Modified by: Vir
- * @Last Modified time: 2021-09-12 17:46:58
+ * @Last Modified time: 2021-09-23 16:50:26
  */
 
+import { checkedBg, SetBackgroundParams } from '@/apis/setting/background';
 import Copyright from '@/components/global/copyright';
 import { PageProps } from '@/typings';
 import { IconButton, Tooltip } from '@material-ui/core';
 import { Bookmarks, Settings } from '@material-ui/icons';
+import classNames from 'classnames';
 import React from 'react';
 import SearchInput from './components/search-input';
 
 const IndexPage: React.FC<PageProps> = ({ history, ...props }) => {
+  const [bg, setBg] = React.useState<SetBackgroundParams>();
+
   const handleSearch = (value: string) => {
     window.open(value);
   };
 
+  const setBackground = () => {
+    const userId = localStorage.getItem('account');
+    const check = checkedBg(userId);
+    if (check) {
+      setBg(check);
+    }
+  };
+
+  React.useEffect(() => {
+    setBackground();
+  }, []);
+
   return (
-    <div className="index-page flex flex-col h-screen">
-      <div className="index-navbar-box bg-gray-500 flex-grow max-h-12 text-right align-middle">
+    <div
+      className="index-page flex flex-col h-screen"
+      style={{ backgroundImage: bg ? `url('${bg?.url}')` : undefined }}
+    >
+      <div className="index-navbar-box flex-grow max-h-12 text-right align-middle">
         <Tooltip title="网址导航">
           <IconButton onClick={() => history.push('/navigation')}>
-            <Bookmarks />
+            <Bookmarks
+              className={classNames({
+                'text-white': !!bg,
+              })}
+            />
           </IconButton>
         </Tooltip>
         <Tooltip title="设置">
           <IconButton onClick={() => history.push('/setting')}>
-            <Settings />
+            <Settings
+              className={classNames({
+                'text-white': !!bg,
+              })}
+            />
           </IconButton>
         </Tooltip>
       </div>
-      <div className="index-logo-box bg-red-500 flex-grow max-h-48 sm:max-h-72"></div>
-      <div className="index-input-box bg-yellow-500 flex-grow max-h-20 flex justify-center items-center">
+      <div className="index-logo-box flex-grow max-h-48 sm:max-h-72"></div>
+      <div className="index-input-box flex-grow max-h-20 flex justify-center items-center">
         <SearchInput placeholder="请输入搜索内容" />
       </div>
-      <div className="index-content-box bg-green-500 flex-grow"></div>
-      <div className="index-copyright-box bg-blue-500 flex-grow max-h-8 text-center leading-8">
+      <div className="index-content-box flex-grow"></div>
+      <div className="index-copyright-box flex-grow max-h-8 text-center leading-8">
         <Copyright />
       </div>
     </div>
