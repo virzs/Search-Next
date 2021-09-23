afe5d7a1a1917b163fb166b8966d7632f6a3f5b3
virzs <2369310227@qq.com>
2021-09-23 16:29:49 +0800

    :sparkles: 网页title动态切换

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
  * @Author: Vir
  * @Date: 2021-09-08 14:22:02
  * @Last Modified by: Vir
- * @Last Modified time: 2021-09-18 16:32:05
+ * @Last Modified time: 2021-09-23 16:27:58
  */
 
 import React, { Suspense } from 'react';
@@ -18,13 +18,14 @@ const Recursive = (routes: Router[]) => {
         path={i.path}
         key={i.path}
         exact={i.exact !== undefined ? i.exact : true}
-        component={(props: any) =>
-          React.createElement(i.component, {
+        component={(props: any) => {
+          document.title = i.title || '';
+          return React.createElement(i.component, {
             ...props,
             route: i,
             children: i.routes ? Recursive(i.routes) : undefined,
-          })
-        }
+          });
+        }}
       />
     );
   });
