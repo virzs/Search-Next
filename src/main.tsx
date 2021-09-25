f5a6b0a4140b317a998eebdc78d8df5b4e448a2f
virzs <2369310227@qq.com>
2021-09-25 16:58:50 +0800

    :sparkles:全局错误监听

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { addAccount } from './apis/auth';
 import { authDefaultData } from './data/account/default';
+import { message } from 'antd';
 
 // 全局初始化事件
 window.addEventListener('DOMContentLoaded', () => {
@@ -14,6 +15,19 @@ window.addEventListener('DOMContentLoaded', () => {
     localStorage.setItem('account', inset._id);
   }
 });
+
+// 错误监听
+window.onerror = function (msg, source, lineno, colno, error) {
+  /* 错误信息（字符串）：message
+      发生错误的脚本URL（字符串）：source
+      发生错误的行号（数字）：lineno
+      发生错误的列号（数字）：colno
+      Error对象（对象）：error
+      https://developer.mozilla.org/zh-CN/docs/Web/API/GlobalEventHandlers/onerror */
+  console.log(msg, source, lineno, colno, error);
+  message.error(msg);
+  return false;
+};
 
 ReactDOM.render(
   <React.StrictMode>
