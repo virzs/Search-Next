861fd02a7d7e274af249dc700dfc87062db202dc
virzs <2369310227@qq.com>
2021-10-03 20:59:12 +0800

    :sparkles: 多语言默认依照系统语言切换

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -2,11 +2,12 @@
  * @Author: Vir
  * @Date: 2021-10-03 18:26:30
  * @Last Modified by: Vir
- * @Last Modified time: 2021-10-03 20:06:09
+ * @Last Modified time: 2021-10-03 20:56:20
  */
 
 import i18n, { Resource } from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import zhCN from './zh-CN.json';
 import enUS from './en-US.json';
 
@@ -23,4 +24,7 @@ const resources: Resource = {
   },
 };
 
-export default i18n.use(initReactI18next).init({ resources, lng: 'zh-CN' });
+export default i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({ resources, fallbackLng: 'zh-CN' });
