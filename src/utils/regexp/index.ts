eb946e3bad39e0b1f3c3cd3c3c7808821d73751b
virzs <2369310227@qq.com>
2021-10-10 14:41:09 +0800

    :beers: :construction: 添加支持 emoji 正则

--- src/utils/regexp/index.ts
+++ src/utils/regexp/index.ts
@@ -2,16 +2,29 @@
  * @Author: Vir
  * @Date: 2021-09-27 17:50:06
  * @Last Modified by: Vir
- * @Last Modified time: 2021-09-27 17:50:27
+ * @Last Modified time: 2021-10-10 14:31:41
  */
 
 // 验证是否为http/https链接
-const pattern =
+const isHttpLinkPattern =
   /(http|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&:/~\+#]*[\w\-\@?^=%&/~\+#])?/;
 
 export const isHttpLink = {
   test: (value: string) => {
-    return pattern.test(value);
+    return isHttpLinkPattern.test(value);
   },
-  pattern,
+  isHttpLinkPattern,
+};
+
+// 验证指定字符中的内容
+export const isEmojiPattern = /:(\S*):/;
+
+export const isEmoji = {
+  test: (value: string) => {
+    return isEmojiPattern.test(value);
+  },
+  match: (value: string) => {
+    return value.match(isEmojiPattern);
+  },
+  isEmojiPattern,
 };
