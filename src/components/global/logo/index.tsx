d0f6c9b067d072a87df3f2d6e331fc31c3059169
virzs <2369310227@qq.com>
2021-10-24 16:22:20 +0800

    :truck: 重命名clock1

--- src/components/global/logo/index.tsx
+++ src/components/global/logo/index.tsx
@@ -2,10 +2,10 @@
  * @Author: Vir
  * @Date: 2021-10-16 22:24:38
  * @Last Modified by: Vir
- * @Last Modified time: 2021-10-16 22:56:38
+ * @Last Modified time: 2021-10-24 16:21:47
  */
 
-import C1 from './digital-clock';
+import C1 from './Clock1';
 import C2 from './Clock2';
 
 export const Clock1 = C1;
