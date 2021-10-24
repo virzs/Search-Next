6c4a437599e6a2c1dbf9ccfe3bff6fc764c73a06
virzs <2369310227@qq.com>
2021-10-24 16:20:57 +0800

    :children_crossing: 增加lab说明

--- src/views/setting/lab/index.tsx
+++ src/views/setting/lab/index.tsx
@@ -2,7 +2,7 @@
  * @Author: Vir
  * @Date: 2021-10-23 15:49:56
  * @Last Modified by: Vir
- * @Last Modified time: 2021-10-23 16:52:47
+ * @Last Modified time: 2021-10-23 22:52:56
  */
 
 import { Router } from '@/config/router';
@@ -10,6 +10,7 @@ import ContentList from '@/pages/setting/components/contentList';
 import ItemCard from '@/pages/setting/components/itemCard';
 import RenderContent from '@/pages/setting/components/renderContent';
 import { PageProps } from '@/typings';
+import { Alert, AlertTitle } from '@material-ui/core';
 import React from 'react';
 
 const Lab: React.FC<PageProps> = ({ history, route, children, ...props }) => {
@@ -24,6 +25,10 @@ const Lab: React.FC<PageProps> = ({ history, route, children, ...props }) => {
       location={history.location as unknown as Location}
       pChildren={children}
     >
+      <Alert severity="info">
+        <AlertTitle>提示</AlertTitle>
+        实验室中的功能均处在开发中，不保证实际发布。
+      </Alert>
       <ContentList>
         {list.map((i) => (
           <ItemCard
