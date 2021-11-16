5ab156da3012824e3ece1129b77f523d8b7a6c7c
virzs <2369310227@qq.com>
2021-11-16 11:37:54 +0800

    :label: chore types

--- src/data/navigation/interface.ts
+++ src/data/navigation/interface.ts
@@ -2,13 +2,64 @@
  * @Author: Vir
  * @Date: 2021-07-25 18:40:38
  * @Last Modified by: Vir
- * @Last Modified time: 2021-11-04 14:49:11
+ * @Last Modified time: 2021-11-16 11:32:17
  */
 
+import {
+  SOFTWARE,
+  ONLINETOOLS,
+  NEWS,
+  SOCIAL,
+  SHOPPING,
+  EMAIL,
+  VIDEO,
+  DIGITAL,
+  PICTURE,
+  COMMUNITY,
+  DEVELOP,
+  ICONS,
+  TOOLS,
+  COMMON,
+  WORK,
+} from './types/classify';
+
+/**
+ * 网站分类
+ * @desc 网站全部分类
+ */
+export type WebsiteClassify =
+  | SOFTWARE
+  | ONLINETOOLS
+  | NEWS
+  | SOCIAL
+  | SHOPPING
+  | EMAIL
+  | VIDEO
+  | DIGITAL
+  | PICTURE
+  | COMMUNITY
+  | DEVELOP
+  | ICONS
+  | TOOLS
+  | COMMON
+  | WORK;
+
+/**
+ * 网站
+ * @type { string } id - 网站id
+ * @type { string } name - 网站名称
+ * @type { string } url - 网站域名
+ * @typeof { WebsiteClassify } classify - 网站分类
+ * @type { string } intro - 简介
+ * @type { string } icon - 图标地址
+ * @type { string } color - 网站主题色
+ * @type { boolean } isShow - 是否显示
+ */
 export interface Website {
   id: string;
   name: string;
   url: string;
+  classify: WebsiteClassify[];
   intro?: string;
   icon?: string;
   color?: string;
@@ -17,6 +68,9 @@ export interface Website {
 
 export type NavigationType = 'website' | 'navigation';
 
+/**
+ * 导航
+ */
 export interface Navigation {
   id: string;
   name: string;
---
+++ src/data/navigation/types/classify.ts
@@ -0,0 +1,96 @@
+/*
+ * @Author: Vir
+ * @Date: 2021-11-16 11:17:47
+ * @Last Modified by: Vir
+ * @Last Modified time: 2021-11-16 11:32:09
+ */
+
+/**
+ * 软件
+ * @desc 网站分类 - 软件
+ */
+export type SOFTWARE = 'software';
+
+/**
+ * 在线工具
+ * @desc 网站分类 - 在线工具
+ */
+export type ONLINETOOLS = 'onlineTools';
+
+/**
+ * 资讯
+ * @desc 网站分类 - 资讯
+ */
+export type NEWS = 'news';
+
+/**
+ * 社交
+ * @desc 网站分类 - 社交
+ */
+export type SOCIAL = 'social';
+
+/**
+ * 购物
+ * @desc 网站分类 - 购物
+ */
+export type SHOPPING = 'shopping';
+
+/**
+ * 邮箱
+ * @desc 网站分类 - 邮箱
+ */
+export type EMAIL = 'email';
+
+/**
+ * 视频
+ * @desc 网站分类 - 视频
+ */
+export type VIDEO = 'video';
+
+/**
+ * 科技数码
+ * @desc 网站分类 - 科技数码
+ */
+export type DIGITAL = 'digital';
+
+/**
+ * 图片
+ * @desc 网站分类 - 图片
+ */
+export type PICTURE = 'picture';
+
+/**
+ * 社区
+ * @desc 网站分类 - 社区
+ */
+export type COMMUNITY = 'community';
+
+/**
+ * 开发
+ * @desc 网站分类 - 开发
+ */
+export type DEVELOP = 'develop';
+
+/**
+ * 图标
+ * @desc 网站分类 - 图标
+ */
+export type ICONS = 'icons';
+
+/**
+ * 工具
+ * @desc 网站分类 - 工具
+ */
+export type TOOLS = 'tools';
+
+/**
+ * 常用
+ * @desc 网站分类 - 常用
+ */
+export type COMMON = 'common';
+
+/**
+ * 工作
+ * @desc 网站分类 - 工作
+ */
+export type WORK = 'work';
