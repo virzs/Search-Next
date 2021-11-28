94524c28b7009829d257ea816dcabc651b2bd900
virzs <2369310227@qq.com>
2021-11-28 14:48:52 +0800

    :sparkles: feat help page

--- src/pages/help/index.tsx
+++ src/pages/help/index.tsx
@@ -1,13 +1,16 @@
 /*
  * @Author: Vir
  * @Date: 2021-11-04 16:10:36
- * @Last Modified by:   Vir
- * @Last Modified time: 2021-11-04 16:10:36
+ * @Last Modified by: Vir
+ * @Last Modified time: 2021-11-08 21:35:50
  */
 import React from 'react';
+import CommitWebsiteMd from '@/data/help/commit_website.md';
+import Markdown from '@/components/global/markdown';
+import 'github-markdown-css/github-markdown.css';
 
 const Help: React.FC = () => {
-  return <div>Help</div>;
+  return <Markdown source={CommitWebsiteMd} />;
 };
 
 export default Help;
