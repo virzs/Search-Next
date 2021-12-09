eb944441a2fd9b2afb0cc6ee7418dab392f03088
virzs <2369310227@qq.com>
2021-12-09 16:01:35 +0800

    :lipstick: chore 帮助页面样式

--- src/pages/help/index.tsx
+++ src/pages/help/index.tsx
@@ -8,9 +8,28 @@ import React from 'react';
 import CommitWebsiteMd from '@/data/help/commit_website.md';
 import Markdown from '@/components/global/markdown';
 import 'github-markdown-css/github-markdown.css';
+import classNames from 'classnames';
+import { css } from '@emotion/css';
+import Copyright from '@/components/global/copyright';
 
 const Help: React.FC = () => {
-  return <Markdown source={CommitWebsiteMd} />;
+  return (
+    <div className="w-full h-screen flex justify-center bg-gray-50 flex-col items-center">
+      <div
+        className={classNames(
+          'bg-white p-4 shadow flex-grow overflow-y-auto mt-4',
+          css`
+            max-width: 800px;
+          `,
+        )}
+      >
+        <Markdown source={CommitWebsiteMd} />
+      </div>
+      <div className="my-2">
+        <Copyright />
+      </div>
+    </div>
+  );
 };
 
 export default Help;
