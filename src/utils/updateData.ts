508bf009dd1243a7e60a0cc0e634dbc9b699bab3
virzs <2369310227@qq.com>
2021-12-21 15:17:01 +0800

    :recycle: chore 数据校验类

--- src/utils/updateData.ts
+++ src/utils/updateData.ts
@@ -10,16 +10,43 @@ import { deepCopy } from './common';
 // 更新默认数据
 
 class UpdateData {
-  constructor() {}
+  source: any; // 模板数据
+  constructor(source: any) {
+    this.source = source;
+  }
 
-  object(source: any = {}, data: any = {}) {
+  private objRecursion(source: any = {}, data: any = {}) {
     const deepData = deepCopy(data);
+    const dataKeys = Object.keys(deepData);
+
+    let newObj: { [x: string]: any } = {};
+
     Object.keys(source).forEach((i) => {
+      // 校验数据源中存在模板数据中不存在的字段时忽略
+      if (!dataKeys.includes(i)) return;
       if (!deepData[i]) {
-        deepData[i] = source[i];
+        // 模板数据中 i 在数据源中不存在时
+        newObj[i] = source[i];
+      } else {
+        // 模板数据中 i 在数据源中存在时
+        newObj[i] = deepData[i];
+      }
+      // 模板数据中 i 为 Object 类型时向下检查
+      if (source[i] instanceof Object) {
+        if (Object.keys(source[i]).length !== Object.keys(deepData[i]).length) {
+          console.log(source[i]);
+          newObj[i] = this.objRecursion(source[i], deepData[i]);
+        }
       }
     });
-    return deepData;
+
+    return newObj;
+  }
+
+  object(data: any = {}) {
+    const result = this.objRecursion(this.source, data);
+
+    return result;
   }
 }
 
