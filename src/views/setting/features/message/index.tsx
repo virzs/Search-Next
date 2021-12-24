d6e72ba4884af1e93fe42c6db94bb9e3d4af7a48
virzs <2369310227@qq.com>
2021-12-24 16:40:08 +0800

    feat(更新提醒设置): :sparkles: 版本更新信息提醒设置

--- src/views/setting/features/message/index.tsx
+++ src/views/setting/features/message/index.tsx
@@ -4,10 +4,52 @@
  * @Last Modified by: Vir
  * @Last Modified time: 2021-12-24 09:56:40
  */
+import { getAuthDataByKey, updateAuthDataByKey } from '@/apis/auth';
+import { Message as AuthMessage } from '@/data/account/interface';
+import ContentList from '@/pages/setting/components/contentList';
+import ItemCard from '@/pages/setting/components/itemCard';
+import { PageProps } from '@/typings';
+import { Switch } from '@material-ui/core';
 import React from 'react';
 
-const Message: React.FC = () => {
-  return <div>Message</div>;
+const Message: React.FC<PageProps> = (props) => {
+  const [update, setUpdate] = React.useState(false);
+  const [messageData, setMessageData] = React.useState({} as AuthMessage);
+  const init = () => {
+    const account = localStorage.getItem('account');
+    const result = getAuthDataByKey(account ?? '', 'message');
+    setUpdate(result.update ?? true);
+    setMessageData(result);
+  };
+
+  const onUpdateSwichChange = (
+    _: React.ChangeEvent<HTMLInputElement>,
+    checked: boolean,
+  ) => {
+    setUpdate(checked);
+    setMessageData({ ...messageData, update: checked });
+    const account = localStorage.getItem('account');
+    updateAuthDataByKey(account ?? '', 'message', {
+      ...messageData,
+      update: checked,
+    });
+  };
+
+  React.useEffect(() => {
+    init();
+  }, []);
+
+  return (
+    <div>
+      <ContentList>
+        <ItemCard
+          title="版本更新提醒"
+          desc="设置版本更新时是否提醒"
+          action={<Switch checked={update} onChange={onUpdateSwichChange} />}
+        />
+      </ContentList>
+    </div>
+  );
 };
 
 export default Message;
