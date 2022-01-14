ab5934247cd6bc64e0f85ae63affa003115283e3
virzs <2369310227@qq.com>
2022-01-14 17:44:30 +0800

    feat(新增组件): :sparkles: 新增 Form FormModal 组件

---
+++ src/components/global/configForm/index.tsx
@@ -0,0 +1,69 @@
+/*
+ * @Author: Vir
+ * @Date: 2022-01-14 16:19:11
+ * @Last Modified by: Vir
+ * @Last Modified time: 2022-01-14 17:16:37
+ */
+import React, { useEffect } from 'react';
+import Form from '@/components/md-custom/form';
+import { FieldValues, RegisterOptions, UseFormReturn } from 'react-hook-form';
+import { TextField } from '@mui/material';
+
+export type FormFieldType = 'text';
+
+export interface FormItemPropType {
+  name: string;
+  label: string;
+  placeholder?: string;
+  type: FormFieldType;
+  required?: boolean;
+  rules?: Omit<RegisterOptions, 'valueAsNumber' | 'valueAsDate' | 'setValueAs'>;
+}
+
+export interface ConfigFormProps {
+  value?: any;
+  form: UseFormReturn<FieldValues, object>;
+  config: FormItemPropType[];
+}
+
+const ConfigForm: React.FC<ConfigFormProps> = (props) => {
+  const { config, value, form } = props;
+  const { Item } = Form;
+  const { reset } = form;
+
+  useEffect(() => {
+    value && reset(value);
+  }, [value]);
+
+  const renderField = (type: FormFieldType, props: any) => {
+    switch (type) {
+      case 'text':
+      default:
+        return <TextField fullWidth variant="outlined" {...props} />;
+    }
+  };
+  return (
+    <Form form={form} size="small">
+      {config.map(
+        ({ type, placeholder, label, rules, required = false, ...i }, j) => {
+          return (
+            <Item
+              label={label}
+              rules={{
+                required: { value: required, message: '请输入' + label },
+                ...rules,
+              }}
+              {...i}
+            >
+              {renderField(type, {
+                placeholder: placeholder ? placeholder : '请输入' + label,
+              })}
+            </Item>
+          );
+        },
+      )}
+    </Form>
+  );
+};
+
+export default ConfigForm;
---
+++ src/components/global/formModal/index.tsx
@@ -0,0 +1,45 @@
+/*
+ * @Author: Vir
+ * @Date: 2022-01-14 15:58:10
+ * @Last Modified by: Vir
+ * @Last Modified time: 2022-01-14 17:24:01
+ */
+import React from 'react';
+import Modal from '@/components/md-custom/dialog/dialog';
+import Form from '@/components/md-custom/form';
+import ConfigForm, { FormItemPropType } from '../configForm';
+
+export interface FormModalProps {
+  open: boolean;
+  title: string;
+  value?: any;
+  config: FormItemPropType[];
+  onCancel: () => void;
+  onOk: (value: any) => void;
+}
+
+const FormModal: React.FC<FormModalProps> = (props) => {
+  const form = Form.useForm();
+  const { title, open, onCancel, onOk, config, value } = props;
+  const { handleSubmit, reset } = form;
+  return (
+    <Modal
+      title={title}
+      open={open}
+      onCancel={() => {
+        onCancel();
+        reset();
+      }}
+      onOk={() => {
+        handleSubmit(onOk, (err) => {
+          console.log(err);
+        })();
+        reset();
+      }}
+    >
+      <ConfigForm form={form} config={config} value={value} />
+    </Modal>
+  );
+};
+
+export default FormModal;
