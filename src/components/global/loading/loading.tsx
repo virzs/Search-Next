a0196384fbc928e97aed71fc8e3a2e9bd71522ce
vir <virs98@outlook.com>
2022-05-04 21:54:05 +0800

    chore(loading): :sparkles: 新增 loading 组件

---
+++ src/components/global/loading/loading.tsx
@@ -0,0 +1,64 @@
+/*
+ * @Author: Vir
+ * @Date: 2022-05-04 21:17:38
+ * @Last Modified by: Vir
+ * @Last Modified time: 2022-05-04 21:53:20
+ */
+import { css, cx, keyframes } from '@emotion/css';
+import React, { FC } from 'react';
+
+const load = keyframes`
+  0%,
+  20%,
+  40%,
+  60%,
+  100% {
+    transform: scaleY(0.5);
+  }
+
+  20% {
+    transform: scaleY(1);
+  }`;
+
+export interface LoadingProps {
+  children?: any;
+  loading?: boolean;
+}
+
+const Loading: FC<LoadingProps> = (props) => {
+  const { loading, children } = props;
+  const icon = (
+    <div className="w-16 h-16 flex gap-1 justify-center items-center">
+      {new Array(5).fill('').map((i, j) => (
+        <i
+          className={cx(
+            'w-1.5 h-10 rounded scale-y-50',
+            css`
+              background-color: #5f5f5f;
+              animation: ${load} 1s ${j * 0.2}s infinite ease-in-out;
+            `,
+          )}
+          key={j}
+        ></i>
+      ))}
+    </div>
+  );
+  return children ? (
+    <div className={cx(loading && 'flex justify-center items-center relative')}>
+      <div className={cx(loading && 'opacity-0')}>{children}</div>
+      {loading && (
+        <div
+          className={cx(
+            'flex absolute top-0 left-0 right-0 bottom-0 justify-center items-center',
+          )}
+        >
+          {icon}
+        </div>
+      )}
+    </div>
+  ) : (
+    icon
+  );
+};
+
+export default Loading;
