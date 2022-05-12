18f49eb5fa684ae471c959ca387868b8b18ebc4c
vir <virs98@outlook.com>
2022-05-12 14:04:34 +0800

    chore(svgicon): :necktie: SvgIcon 优化

--- src/components/global/svgIcon.tsx
+++ src/components/global/svgIcon.tsx
@@ -2,7 +2,7 @@
  * @Author: Vir
  * @Date: 2022-05-11 10:11:06
  * @Last Modified by: Vir
- * @Last Modified time: 2022-05-11 10:39:57
+ * @Last Modified time: 2022-05-11 14:40:47
  */
 import React, { FC } from 'react';
 
@@ -10,15 +10,24 @@ export interface SvgIconProps {
   name: any;
   prefix?: string;
   color?: string;
+  width?: number;
+  height?: number;
 }
 
 const SvgIcon: FC<SvgIconProps> = (props) => {
-  const { name, prefix = 'icon', color = '#333', ...rest } = props;
+  const {
+    name,
+    prefix = 'icon',
+    color = '#333',
+    width = 24,
+    height = 24,
+    ...rest
+  } = props;
 
   const symbolId = `#${prefix}-${name}`;
 
   return (
-    <svg {...rest} aria-hidden="true">
+    <svg {...rest} width={width} height={height} aria-hidden="true">
       <use href={symbolId} fill={color} />
     </svg>
   );
