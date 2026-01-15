eedfb0d5656902540dd8c8c3fc3de0d111399505
vir <virs98@outlook.com>
2026-01-15 15:09:49 +0800

    feat(主题): 为主题和壁纸菜单项添加图标
    为主题和壁纸菜单项添加了 RemixIcon 图标，提升视觉体验和用户交互

--- src/pages/index/components/default-apps/theme/index.tsx
+++ src/pages/index/components/default-apps/theme/index.tsx
@@ -2,6 +2,12 @@ import { AppRoutedOverlay } from "@/components";
 import { FC } from "react";
 import ThemeView from "./views/theme";
 import WallpaperView from "./views/wallpaper";
+import {
+  RiLandscapeFill,
+  RiLandscapeLine,
+  RiTShirtFill,
+  RiTShirtLine,
+} from "@remixicon/react";
 
 const ThemeModalRoute: FC = () => {
   return (
@@ -12,8 +18,20 @@ const ThemeModalRoute: FC = () => {
       sidebarProps={{
         header: <div className="text-2xl font-bold tracking-tight">个性化</div>,
         menuItems: [
-          { key: "theme", label: "主题", path: "/theme" },
-          { key: "wallpaper", label: "壁纸", path: "/theme/wallpaper" },
+          {
+            key: "theme",
+            label: "主题",
+            path: "/theme",
+            icon: <RiTShirtLine size={16} />,
+            activeIcon: <RiTShirtFill size={16} />,
+          },
+          {
+            key: "wallpaper",
+            label: "壁纸",
+            path: "/theme/wallpaper",
+            icon: <RiLandscapeLine size={16} />,
+            activeIcon: <RiLandscapeFill size={16} />,
+          },
         ],
       }}
       outletWrapperClassName="h-full w-full overflow-auto p-6"
