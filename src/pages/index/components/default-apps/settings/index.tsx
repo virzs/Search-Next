23ba0f05f2fade44bcf053017c707fd9c8b1633a
vir <virs98@outlook.com>
2026-01-20 15:07:31 +0800

    feat(settings): 为设置菜单项添加激活状态图标
    为账号、个性化、第三方服务等设置菜单项添加了激活状态下的图标显示，提升用户交互体验

--- src/pages/index/components/default-apps/settings/index.tsx
+++ src/pages/index/components/default-apps/settings/index.tsx
@@ -1,5 +1,15 @@
 import { FC } from "react";
-import { RiBrushLine, RiInbox2Fill, RiUserFill } from "@remixicon/react";
+import {
+  RiBrushFill,
+  RiBrushLine,
+  RiGlobalFill,
+  RiInbox2Fill,
+  RiInbox2Line,
+  RiInformationLine,
+  RiKeyLine,
+  RiUserFill,
+  RiUserLine,
+} from "@remixicon/react";
 import { RiGlobalLine, RiInformationFill, RiKeyFill } from "@remixicon/react";
 import { AppRoutedOverlay } from "@/components";
 import { Outlet, useLocation } from "react-router";
@@ -58,37 +68,43 @@ const SettingsModalRoute: FC = () => {
             key: "account",
             label: "账号",
             path: settingsRoute.path.account,
-            icon: <RiUserFill size={16} />,
+            icon: <RiUserLine size={16} />,
+            activeIcon: <RiUserFill size={16} />,
           },
           {
             key: "personalization",
             label: "个性化",
             path: settingsRoute.path.personalization,
             icon: <RiBrushLine size={16} />,
+            activeIcon: <RiBrushFill size={16} />,
           },
           {
             key: "third-party",
             label: "第三方服务",
             path: settingsRoute.path.thirdParty,
-            icon: <RiKeyFill size={16} />,
+            icon: <RiKeyLine size={16} />,
+            activeIcon: <RiKeyFill size={16} />,
           },
           {
             key: "language",
             label: "语言",
             path: settingsRoute.path.language,
             icon: <RiGlobalLine size={16} />,
+            activeIcon: <RiGlobalFill size={16} />,
           },
           {
             key: "backup",
             label: "备份与恢复",
             path: settingsRoute.path.backup,
-            icon: <RiInbox2Fill size={16} />,
+            icon: <RiInbox2Line size={16} />,
+            activeIcon: <RiInbox2Fill size={16} />,
           },
           {
             key: "about",
             label: "关于",
             path: settingsRoute.path.about,
-            icon: <RiInformationFill size={16} />,
+            icon: <RiInformationLine size={16} />,
+            activeIcon: <RiInformationFill size={16} />,
           },
         ],
       }}
